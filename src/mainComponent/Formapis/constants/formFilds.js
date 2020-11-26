1de5ab0407e4a1a046616948a826654f154467e0
VIJAY A <vijayao2015@gmail.com>
2020-11-26 13:30:09 +0530

    Frist Commit

---
+++ src/mainComponent/Formapis/constants/formFilds.js
@@ -0,0 +1,8 @@
+export const config = [
+    {  
+        field: [
+            {label:"Field " , type:"text" , value:"name",id:'field_1'},
+            {label:"Value " , type:"text" , value:"Add",id:'value_1'},
+             ]
+    },
+   ]
